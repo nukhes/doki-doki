6bad2228046845adb115eb3877fa785262ff70cd
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-04-12 16:48:15 +0000

    trying to fix

--- api/index.js
+++ api/index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
 //settings
 app.use(express.static('./frontend'));
-app.set("port", process.env.PORT || 80);
-
-
 
 //routes
 app.use(require('./routes/index'))
 
 //starting the server
-app.listen((app.get('port')), function () {
-    console.log('listening on 80')
+app.listen((app.get(80)), function () {
+    console.log('Listening on port 80')
 })
