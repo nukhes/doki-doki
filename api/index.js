22443b4ae2bdfe1700e603878314b3f53cba94e7
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-10 20:10:56 +0000

    simplification of index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,11 @@
+// Starts server in port 80
 const express = require('express');
 const app = express();
-const path = require('path');
 
-//settings
 app.use(express.static('./frontend'));
 
-//routes
 app.use(require('./routes/index'))
 
-//starting the server
 app.listen((app.get(80)), function () {
     console.log('Listening on port 80')
 })
