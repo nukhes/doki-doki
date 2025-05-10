dff138bda3e40f45f11d5d58df52c8db872af0fb
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-10 19:52:36 +0000

    codewars refactored

--- codewars.js
+++ codewars.js
@@ -1,82 +1,70 @@
-//Sábado, 31 de Julio
-function accum(str){
-    let fin = str[0].toUpperCase();
- //
-    function pushMore(x,num){
-        let cantidad = "-" + x.toUpperCase();
-        for (let i = 1; i < num; i++) {
-            cantidad += x.toLowerCase();
+// 31st July - Mumbling function
+function accum(str) {
+    let result = str[0].toUpperCase();
+    
+    function buildSegment(char, count) {
+        let segment = "-" + char.toUpperCase();
+        for (let i = 1; i < count; i++) {
+            segment += char.toLowerCase();
         }
-        return cantidad
+        return segment;
     }
-//
 
     for (let i = 1; i < str.length; i++) {
-        const element = str[i]
-        let resul = pushMore(element, i+1)
-        fin += resul;
+        const char = str[i];
+        result += buildSegment(char, i + 1);
     }
-    return fin
+    return result;
 }
-//domingo 1 de agosto
 
+// 1st August - Get Middle Character
 function getMiddle(s) {
-    let splitted = s.split('');
-    if (splitted.length % 2 == 0) {
-
-        return (splitted[(splitted.length / 2) - 1].concat(splitted[splitted.length / 2]))
-    }
-    else {
-        return (splitted[Math.floor(splitted.length / 2)])
-    }
+    const middle = Math.floor(s.length / 2);
+    return s.length % 2 === 0 
+        ? s.slice(middle - 1, middle + 1) 
+        : s[middle];
 }
 
-//Martes 3 de Agosto
-
-function solution(number){
-    let res = 0;
-    for (let index = 0; index < number; index++) {
-        if(index % 3 == 0 || index % 5 == 0) res += index;
+// 3rd August - Multiples of 3 or 5
+function sumMultiples(number) {
+    let sum = 0;
+    for (let i = 0; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) sum += i;
     }
-    return res
+    return sum;
 }
 
-//jueves 5 de Agosto
-
-function list(names) {
-    let resul = "";
+// 5th August - Format Names List
+function formatNamesList(names) {
+    let result = "";
     for (let i = 0; i < names.length; i++) {
-        const element = names[i];
-        resul += element.name;
-        if(names[i+1] === undefined) return resul;
-        else if(names[i+2] === undefined) resul += " & ";
-        else resul += ", ";
+        result += names[i].name;
+        if (i === names.length - 1) return result;
+        result += (i === names.length - 2) ? " & " : ", ";
     }
-}
-//martes 10 de agosto fua sera que estuve boludeando
-
-function disemvowel(str){
-    let newStr = "";
-    let lowered = str.toLowerCase();
-    for (let i = 0; i < str.length; i++) {
-        if(lowered[i] === "a" || lowered[i] === "e" || lowered[i] === "i" || lowered[i] === "o" || lowered[i] === "u") continue;
-        else newStr += str[i];
-    }
-    return newStr;
+    return result;
 }
 
-//miercoles 11 de Agosto
+// 10th August - Disemvowel
+function disemvowel(str) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let result = "";
+    for (const char of str) {
+        if (!vowels.has(char.toLowerCase())) {
+            result += char;
+        }
+    }
+    return result;
+}
 
+// 11th August - Isogram Check
 function isIsogram(str) {
-    if(str === '' || str[1] === undefined)return true;
-    let dictionary = {}
-    let lowered = str.toLowerCase();
-    for (let i = 0; i < lowered.length; i++) {
-        if(dictionary[lowered[i]]) return false;
-        else dictionary[lowered[i]] = true;
+    if (str.length <= 1) return true;
+    const seenChars = {};
+    const lowerStr = str.toLowerCase();
+    for (const char of lowerStr) {
+        if (seenChars[char]) return false;
+        seenChars[char] = true;
     }
     return true;
 }
-
-// 
-
